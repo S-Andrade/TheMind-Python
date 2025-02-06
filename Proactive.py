cb7f8c07a54ef59e6148ffe2142bd7ac2d87decc
S-Andrade <sandraandrade@ua.pt>
2025-02-06 02:44:37 +0000

    tonight

--- Proactive.py
+++ Proactive.py
@@ -272,7 +272,7 @@ def main():
     playcard = ""
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
-    s.connect(('127.0.0.1', 50001))
+    s.connect(('192.168.1.169', 50001))
     msgid = "Player 2" 
     s.send(msgid.encode())
 
